a3f2f9672031912445a0a41e7195dcc29e9724f8
IDontKnow-XD <102936326+IDontKnow-XD@users.noreply.github.com>
2023-06-08 20:43:04 +0700

    Vid-to-img1

--- Tracker/Assets/Script/FilePicker.cs
+++ Tracker/Assets/Script/FilePicker.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Video;
 using TMPro;
 
 public class FilePicker : MonoBehaviour
 {
     private string Finalpath;
+    public VideoPlayer videoPlayer;
     public TextMeshProUGUI Vid1_name;
     public TextMeshProUGUI Vid2_name;
     public string Vid1_path;
@@ -36,12 +38,14 @@ public class FilePicker : MonoBehaviour
         Import();
         Vid1_path = Finalpath;
         Vid1_name.text = Output.ToString();
+        videoPlayer.url = Vid1_path;
     }
     public void Import2()
     {
         Import();
         Vid2_path = Finalpath;
         Vid2_name.text = Output.ToString();
+        videoPlayer.url = Vid2_path;
     }
     void Start(){
         Vid1_name.text = "+";
--- Tracker/Assets/Script/Frame_ext.cs
+++ Tracker/Assets/Script/Frame_ext.cs
@@ -8,33 +8,54 @@ public class Frame_ext : MonoBehaviour
 {
     // Start is called before the first frame update
     public RenderTexture renderTexture;
+    public Vid_Ended vid_Ended;
+    private int i;
+    private short button;
+    public void button1()
+    {
+        button = 1;
+    }
+    public void button2()
+    {
+        button = 2;
+    }
     void Start()
     {
-        Thread.Sleep(2000);
-        Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
-
-        // Read the pixel data from the Render Texture into the Texture2D
-        RenderTexture.active = renderTexture;
-        texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-        texture.Apply();
-
-        // Convert the Texture2D to a byte array
-        byte[] bytes = texture.EncodeToPNG();
-
-        // Save the image to a file (optional)
-        string path = Path.Combine(Application.persistentDataPath, @"C:\Users\User\Documents\GitHub\Repository-Tracker\Tracker\Assets\Frames\test1.png");
-        File.WriteAllBytes(path, bytes);
-        Debug.Log("Image saved to: " + path);
-
-        // Don't forget to clean up
-        RenderTexture.active = null;
-        Destroy(texture);
-        Debug.Log("Done");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (vid_Ended.isVideoPlaying && vid_Ended.Check <= 2)
+        {
+            if (i != 0)
+            {
+                Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+
+                // Read the pixel data from the Render Texture into the Texture2D
+                RenderTexture.active = renderTexture;
+                texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+                texture.Apply();
+
+                // Convert the Texture2D to a byte array
+                byte[] bytes = texture.EncodeToPNG();
+
+                // Save the image to a file (optional)
+                string path = Path.Combine(Application.dataPath, @"Frames\Img_" + button + "_" + i.ToString() + ".png");
+                File.WriteAllBytes(path, bytes);
+                Debug.Log("Image saved to: " + path);
+
+                // Don't forget to clean up
+                RenderTexture.active = null;
+                Destroy(texture);
+                Debug.Log("Done");
+            }
+            i++;
+        }
+        else
+        {
+            i = 0;
+        }
     }
 }
