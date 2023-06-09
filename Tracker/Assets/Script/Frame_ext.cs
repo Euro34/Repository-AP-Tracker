b309e8a3b33d1d099c292a5aa609dfcd92c54b7e
IDontKnow-XD <102936326+IDontKnow-XD@users.noreply.github.com>
2023-06-09 07:16:52 +0700

    Vid-img2
    pretty much works about 20fps

--- Tracker/Assets/Script/Frame_ext.cs
+++ Tracker/Assets/Script/Frame_ext.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Threading;
+using Unity.VisualScripting;
+using System;
 
 public class Frame_ext : MonoBehaviour
 {
     // Start is called before the first frame update
     public RenderTexture renderTexture;
     public Vid_Ended vid_Ended;
-    private int i;
+    private int i = 0;
     private short button;
+    public List<byte[]> byteslist = new List<byte[]>();
     public void button1()
     {
         button = 1;
@@ -21,41 +24,58 @@ public class Frame_ext : MonoBehaviour
     }
     void Start()
     {
-        
+        StartCoroutine(ExtractFramesCoroutine());
     }
 
     // Update is called once per frame
+    private IEnumerator ExtractFramesCoroutine()
+    {
+        while (true)
+        {
+            if (vid_Ended.isVideoPlaying && vid_Ended.Check <= 2)
+            {
+                if (i > 1)
+                {
+                    Texture2D texture = new Texture2D(480, 270, TextureFormat.RGB24, false);
+
+                    // Read the pixel data from the Render Texture into the Texture2D
+                    RenderTexture.active = renderTexture;
+                    texture.ReadPixels(new Rect(0, 0, 480, 270), 0, 0);
+                    texture.Apply();
+
+                    // Convert the Texture2D to a byte array
+                    byte[] bytes = texture.EncodeToPNG();
+
+                    byteslist.Add(bytes);
+
+                    // Don't forget to clean up
+                    RenderTexture.active = null;
+                    Destroy(texture);
+                    Debug.Log(byteslist.Count);
+                }
+                i++;
+            }
+            else
+            {
+                if (i > 1)
+                {
+                    for(i = 0; i < byteslist.Count; i++)
+                    {
+                        byte[] bytes = byteslist[i];
+                        // Save the image to a file (optional)
+                        string path = Path.Combine(Application.dataPath, @"Frames/Img_" + button + "_" + (i - 1).ToString() + ".png");
+                        File.WriteAllBytes(path, bytes);
+                        Debug.Log("Image saved to: " + path);
+                    }
+                    i = 0;
+                }
+            }
+            Thread.Sleep(1);
+            yield return null;
+        }
+    }
     void Update()
     {
-        if (vid_Ended.isVideoPlaying && vid_Ended.Check <= 2)
-        {
-            if (i != 0)
-            {
-                Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
 
-                // Read the pixel data from the Render Texture into the Texture2D
-                RenderTexture.active = renderTexture;
-                texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-                texture.Apply();
-
-                // Convert the Texture2D to a byte array
-                byte[] bytes = texture.EncodeToPNG();
-
-                // Save the image to a file (optional)
-                string path = Path.Combine(Application.dataPath, @"Frames\Img_" + button + "_" + i.ToString() + ".png");
-                File.WriteAllBytes(path, bytes);
-                Debug.Log("Image saved to: " + path);
-
-                // Don't forget to clean up
-                RenderTexture.active = null;
-                Destroy(texture);
-                Debug.Log("Done");
-            }
-            i++;
-        }
-        else
-        {
-            i = 0;
-        }
     }
 }
