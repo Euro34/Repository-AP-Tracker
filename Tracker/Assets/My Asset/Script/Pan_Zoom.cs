d74c276cee7f6744c9e91f838f449349db125289
Euro34 <133733253+Euro34@users.noreply.github.com>
2023-07-06 17:59:05 +0700

    pan & zoom + some bug

--- Tracker/Assets/My Asset/Script/Pan_Zoom.cs
+++ Tracker/Assets/My Asset/Script/Pan_Zoom.cs
@@ -1,84 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class Pan_Zoom : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class Pan_Zoom : MonoBehaviour
 {
-    private bool _isDragging;
-    private bool Slider_Drag;
-    private float _currentScale;
-    public float minScale, maxScale;
-    public Slider _slider;
-    private float _temp;
-    private float _scalingRate = 0.01f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _currentScale = transform.localScale.x;
-        StartCoroutine(Check());
-    }
-
-    private IEnumerator Check()
-    {
-        while (true)
-        {
-            if (_isDragging)
-            {
-                transform.localScale = new Vector2(_currentScale, _currentScale);
-                float distance = Vector3.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
-                if (_temp > distance)
-                {
-                    _currentScale -=  (_temp - distance) * _scalingRate/ (Time.deltaTime);
-                }
-
-                else if (_temp < distance)
-                {
-                    _currentScale += (distance - _temp) * _scalingRate / (Time.deltaTime);
-                }
-                if (_currentScale < minScale)
-                {
-                    _currentScale = minScale;
-                }
-                if (_currentScale >= maxScale)
-                {
-                    _currentScale = maxScale;
-                }
-                _temp = distance;
-                _slider.value = _currentScale;
-            }
-            else
-            {
-                _temp = 0;
-            }
-            if(Slider_Drag)
-            {
-                _currentScale = _slider.value;
-            }
-            yield return null;
-        }
-    }
-    // Update is called once per frame
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        if (Input.touchCount == 2)
-        {
-            _isDragging = true;
-        }
-    }
+    private UnityEngine.Vector3 initialScale;
+    public Zoom_Slider zoom_slider;
+    float Zoom;
 
 
-    public void OnPointerUp(PointerEventData eventData)
+    [SerializeField]
+    private int maxZoom = 50;
+
+    private void Awake()
     {
-        _isDragging = false;
+        initialScale = transform.localScale;
     }
-    public void SliderPointerDown()
+
+    public void SetScale()
     {
-        Slider_Drag = true;
+        Zoom = (zoom_slider.Zoom)/100;
+        var desiredScale = initialScale * Zoom;
+        desiredScale = ClampDesiredScale(desiredScale);
+        transform.localScale = desiredScale;
     }
-    public void SliderPointerUp()
+
+    private UnityEngine.Vector3 ClampDesiredScale(Vector3 desiredScale)
     {
-        Slider_Drag = false;
+        desiredScale = Vector3.Max(initialScale/2, desiredScale);
+        desiredScale = Vector3.Min(initialScale * maxZoom, desiredScale);
+        return desiredScale;
     }
 }
---
+++ Tracker/Assets/My Asset/Script/Zoom_Slider.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class Zoom_Slider: MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool isDragging;
+    private bool SliderDrag;
+    private float currentScale;
+    public float minScale, maxScale;
+    public Slider slider;
+    private float temp;
+    private float scalingRate = 0.01f;
+    public float Zoom;
+    public TextMeshProUGUI Zoom_txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScale = transform.localScale.x;
+        StartCoroutine(Check());
+    }
+
+    private IEnumerator Check()
+    {
+        while (true)
+        {
+            if (isDragging)
+            {
+                transform.localScale = new Vector2(currentScale, currentScale);
+                float distance = Vector3.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+                if (temp > distance)
+                {
+                    currentScale -=  (temp - distance) * scalingRate/ (Time.deltaTime);
+                }
+
+                else if (temp < distance)
+                {
+                    currentScale += (distance - temp) * scalingRate / (Time.deltaTime);
+                }
+                if (currentScale < minScale)
+                {
+                    currentScale = minScale;
+                }
+                if (currentScale >= maxScale)
+                {
+                    currentScale = maxScale;
+                }
+                temp = distance;
+                slider.value = currentScale;
+            }
+            else
+            {
+                temp = 0;
+            }
+            if(SliderDrag)
+            {
+                currentScale = slider.value;
+            }
+            Zoom = (Mathf.Pow(slider.value, 2) / 2);
+            Zoom_txt.text = Zoom.ToString() + " %";
+            yield return null;
+        }
+    }
+    // Update is called once per frame
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (Input.touchCount == 2)
+        {
+            isDragging = true;
+        }
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging = false;
+    }
+    public void SliderPointerDown()
+    {
+        SliderDrag = true;
+    }
+    public void SliderPointerUp()
+    {
+        SliderDrag = false;
+    }
+}
