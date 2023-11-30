bf7ab5416d3ad26134920c2d25f8338743a307d4
IDontKnow-XD <102936326+IDontKnow-XD@users.noreply.github.com>
2023-11-30 13:42:15 +0700

    UI_fix_code

--- Tracker/Assets/My Asset/Script/Switch_Scene_Relative_Point.cs
+++ Tracker/Assets/My Asset/Script/Switch_Scene_Relative_Point.cs
@@ -5,10 +5,15 @@ public class Switch_Scene_Relative_Point : MonoBehaviour
 {
     public void to_edit_Rela_Point()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
     }
     public void Back()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(0);
+
+    }
+    public void to_edit_Rela_Object()
+    {
+        SceneManager.LoadScene(2);
     }
 }
